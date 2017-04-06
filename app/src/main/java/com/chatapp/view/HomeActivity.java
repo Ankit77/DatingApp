dea8a6f0a30593be1a89fafa7261a28264e2bfb9
ANKIT <adkhatri77@gmail.com>
2017-04-06 07:39:28 +0530

    dfd

--- app/src/main/java/com/chatapp/view/HomeActivity.java
+++ app/src/main/java/com/chatapp/view/HomeActivity.java
@@ -3,7 +3,11 @@ package com.chatapp.view;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.chatapp.R;
 import com.chatapp.fragment.SuggestionFragment;
@@ -14,13 +18,30 @@ import com.chatapp.util.Utils;
  */
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        configureNavigationDrawer();
+        configureToolbar();
+
         SuggestionFragment suggestionFragment = new SuggestionFragment();
         getFragmentManager().beginTransaction().add(R.id.activity_home_container, suggestionFragment, SuggestionFragment.class.getSimpleName()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_action_menu_white);
+        actionbar.setDisplayHomeAsUpEnabled(true);
     }
 
 }
