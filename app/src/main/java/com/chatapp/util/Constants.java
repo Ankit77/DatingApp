dad8fe482d70bbaecc9f17fc14076d64084415e4
ANKIT <adkhatri77@gmail.com>
2017-04-12 02:21:55 +0530

    implement facebook

--- app/src/main/java/com/chatapp/util/Constants.java
+++ app/src/main/java/com/chatapp/util/Constants.java
@@ -2,24 +2,11 @@ package com.chatapp.util;
 
 
 public class Constants {
-    public static final long MAX_CLICK_INTERVAL = 1000;
-    public static final int SPLASH_TIME_OUT = 1500;
 
-    public static final int IMAGE = 1;
-    public static final int VIDEO = 2;
-    public static final int TEXT = 3;
+    public static final int MAX_DISTANCE = 200;
+    public static final int MIN_AGE = 18;
+    public static final int MAX_AGE = 55;
 
-    public static final int PREVIEW_IMAGE = 0;
-    public static final int PREVIEW_VIDEO = 1;
-    public static final int PREVIEW_TEXT = 2;
-
-    public static final int PREVIEW_FILE = 5;
-    public static final int PREVIEW_NEWS = 419;
-    public static final int PREVIEW_PHOTO = 422;
-    public static final int PREVIEW_MEDIA = 422;
-    public static final int PREVIEW_DOCUMENT = 80;
-
-    public static final String LANGUAGE_EN_CODE = "1";
     public static final String LANGUAGE_AR_CODE = "2";
     public static final String LANGUAGE_FR_CODE = "3";
 
@@ -31,58 +18,6 @@ public class Constants {
     public static final int FONT_MEDIUM = 2;
     public static final int FONT_LARGE = 3;
 
-    public static final String DEFAULT_HOME_PAGE_ID = "-1";
-    public static final String AUTH_TYPE_USER = "0";
-    public static final String AUTH_TYPE_ANONYMOUS = "1";
-
-    public static final int CALENDER_ANYTIME = 0;
-
-    public static final int MAX_LOAD_IMAGE_HEIGHT = 1000;
-    public static final int MAX_LOAD_IMAGE_WIDTH = 1500;
-
-    public static final int DEFAULT_LOAD_IMAGE_HEIGHT = 1000;
-    public static final int DEFAULT_LOAD_IMAGE_WIDTH = 1000;
-
-    public static final String KEY_CATEGORY_NAMES = "KEY_CATEGORY_NAMES";
-    public static final String KEY_CATEGORY_LIST = "KEY_CATEGORY_LIST";
-
-    public static final int KEY_TTS_CHARECTER_LIMIT = 3900;
-
-    // class id for screens to load from side menu
-    public static final String CLASS_ID_MESSAGE = "401";
-    public static final String CLASS_ID_EVENTS = "600";
-    public static final String CLASS_ID_MEDIA = "75";
-    public static final String CLASS_ID_CHANNEL = "52";
-    public static final String CLASS_ID_FILES_1 = "47";
-    public static final String CLASS_ID_FILES_2 = "50";
-
-    //    Route To User
-    public static final int DEPARTMENT_INBOX = 0;
-    public static final int USER_IN_DEPARTMENT = 1;
-
-    public static final String VIDEO_TYPE_ID = "442";
-    public static final String IMAGE_EXTENSION = ".jpg";
-    public static final String VIDEO_EXTENSION = ".mp4";
-
-    public static final int COPY = 0;
-    public static final int MOVE = 1;
-    public static final int COPYWITHRELATION = 3;
-    public static final int MAX_LENGTH_SMS = 3500;
-
-    // Priority
-    public static final String PRIORITY_HIGH = "0";
-    public static final String PRIORITY_NORMAL = "1";
-    public static final String PRIORITY_LOW = "2";
-
-    //AUTOSAVE
-    public static final int AUTOSAVE_INTERVAL = 1000 * 60;
-
-    //EVENT STATUS
-    public static final int STATUS_NOT_STARTED = 0;
-    public static final int STATUS_IN_PROGRESS = 1;
-    public static final int STATUS_COMPLETED = 2;
-    public static final int STATUS_PENDING = 3;
-    public static final int STATUS_CANCELLED = 4;
 
 }
 
